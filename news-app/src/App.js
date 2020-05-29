5bf0468b27b325aaf7aa9108790cbbd6fa7e0f93
Anil <anilbansal091@gmail.com>
2020-05-29 10:24:36 +0530

    Improved Fetch Functionality

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-//import  {getNews}  from './news';
+import  {getNews}  from './news';
 import Display from './Display';
 import Head from './head';
 
@@ -34,15 +34,11 @@ class App extends Component{
   }
 
   fetchnews(){
-    const url=`https://newsapi.org/v2/top-headlines?country=${this.state.country}&apiKey=ad23c45e8dbf4c418fc72871384d9ec5`;
-    fetch(url)
-    .then(response=> response.json())
-    .then(result=> result.articles)
+    getNews(this.state.country)
     .then(articles=> {
       this.setState({articles: [...this.state.articles,...articles]});
       this.setState({page: this.state.page+1});
       this.setState({is_loading: false});
-      //console.log(articles.length);
     })
     .catch(error=>{
       console.log(error);

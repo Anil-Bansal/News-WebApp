c793866ea5edacabfdbcfcc2f0ef2b9711cb0c64
Vinayak Aggarwal <f20170008@goa.bits-pilani.ac.in>
2020-05-31 19:11:49 +0530

    Add Hover in News

--- news-app/src/Post.js
+++ news-app/src/Post.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 
-const Post=(props)=>(
-    <div>
-        <Card style={{width: '24rem'}} onClick={()=>window.open(props.url,'_blank')  }>
-            <Card.Img variant="top" src={props.imageurl} />
-            <Card.Body>
-                <Card.Title>{props.title}</Card.Title>
-                <Card.Text>{props.description}</Card.Text>
-            </Card.Body>    
-        </Card>
-    </div>    
+class Post extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          backg: "light",
+          textColor: 'dark'
+        };
+      }
 
-);
+    enter = () =>{
+        this.setState({
+            backg: "info",
+            textColor: 'white'
+    })
+    }  
+    leave = () =>{
+        this.setState({
+            backg: "light",
+            textColor: 'dark'
+        })
+    }  
+    render(){
+    return(
+        <div>
+            <Card bg={this.state.backg}
+                onMouseEnter={this.enter}
+                onMouseLeave={this.leave}
+                style={{width: '24rem'}}
+                text={this.state.textColor}
+                onClick={()=>window.open(this.props.url,'_blank')  }>
+                <Card.Img variant="top" src={this.props.imageurl} />
+                <Card.Body>
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Text>{this.props.description}</Card.Text>
+                </Card.Body>    
+            </Card>
+        </div>    
+    )}
+}
 
 export default Post;

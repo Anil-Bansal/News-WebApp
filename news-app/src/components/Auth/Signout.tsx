b9a08feb1a92dabf2a3a7934016489ca850f586c
Anil <anilbansal091@gmail.com>
2020-06-07 18:03:42 +0530

    Added Redux to Signout and fixed the issues

--- news-app/src/components/Auth/Signout.tsx
+++ news-app/src/components/Auth/Signout.tsx
@@ -2,25 +2,48 @@ import React from 'react';
 import { withFirebase } from '../Firebase';
 import {withRouter,useHistory} from 'react-router-dom';
 import Button from  'react-bootstrap/Button';
+import {connect} from 'react-redux';
+import * as actiontypes from '../Redux/Actions';
 
-
-const SignOutButton = (props) => {
-  const history = useHistory();
-
-  const signOut= () =>
-  {
-    props.firebase.doSignOut()
-    history.push('/SignIn')
+class SignOutButton extends React.Component{
+  constructor(props){
+    super(props);
+    this.signOut=this.signOut.bind(this);
   }
 
-  return (
-    <Button variant='danger'
-      type='submit' onClick={signOut}>
-        Sign Out
-    </Button>
-  )
+  signOut(){
+    this.props.firebase.doSignOut();
+    this.props.setLoginStatus(false);
+    this.props.setUserId(null);
+    this.props.setCookieLoad(false);
+    this.props.history.push('/SignIn');
+  }
+
+  render(){
+    return (
+      <Button variant='danger'
+        type='submit' onClick={this.signOut}>
+          Sign Out
+      </Button>
+    )
+  }
+
 }
 
 
+const mapStateToProps=(state: Object)=>{
+  return{
+    isLoggedIn: state.isLoggedIn,
+    uid: state.uid,
+  };
+}
+
+const mapDispatchToProps=dispatch=>{
+  return{
+    setLoginStatus: (val)=>dispatch(actiontypes.setLoginStatus(val)),
+    setUserId: (val)=>dispatch(actiontypes.setUserId(val)),
+    setCookieLoad: (val)=>dispatch(actiontypes.setCookieLoad(val))
+  };
+}
  
-export default withRouter(withFirebase(SignOutButton));
+export default connect(mapStateToProps,mapDispatchToProps)(withRouter(withFirebase(SignOutButton)));

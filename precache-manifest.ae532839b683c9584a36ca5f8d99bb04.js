4b70b3dea66c09db7cb7a61bd4fa1dd408f410bf
Vinayak Aggarwal <f20170008@goa.bits-pilani.ac.in>
2020-06-12 19:37:21 +0530

    Updates

---
+++ precache-manifest.ae532839b683c9584a36ca5f8d99bb04.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6c560475471a730e71287c18a6d29e",
+    "url": "/News-WebApp/index.html"
+  },
+  {
+    "revision": "f76a41bbe570c645f566",
+    "url": "/News-WebApp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cb5742525fd4e2f9b6aa",
+    "url": "/News-WebApp/static/css/main.4f52b9b6.chunk.css"
+  },
+  {
+    "revision": "f76a41bbe570c645f566",
+    "url": "/News-WebApp/static/js/2.7f3f86bc.chunk.js"
+  },
+  {
+    "revision": "cb57e484b6a7ed6b1262a239fd5cdb6a",
+    "url": "/News-WebApp/static/js/2.7f3f86bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb5742525fd4e2f9b6aa",
+    "url": "/News-WebApp/static/js/main.cb9af49d.chunk.js"
+  },
+  {
+    "revision": "9d7835432c6c088c2e8f",
+    "url": "/News-WebApp/static/js/runtime-main.2b9d1cba.js"
+  },
+  {
+    "revision": "d4ab732902d6a5f1f1c7a293560f803b",
+    "url": "/News-WebApp/static/media/Anil.d4ab7329.jpg"
+  },
+  {
+    "revision": "5a157650c1f002042b435e008feca990",
+    "url": "/News-WebApp/static/media/Vinayak.5a157650.jpeg"
+  }
+]);
